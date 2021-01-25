d049d3ec7b4a674b55d896330b7f16c444620cef
edsouthall <51732646+edsouthall@users.noreply.github.com>
2021-01-25 15:57:07 +1100

    Add files via upload
    Function Description
    
    Complete the plusMinus function in the editor below.
    
    plusMinus has the following parameter(s):
    
    int arr[n]: an array of integers
    Print
    Print the ratios of positive, negative and zero values in the array. Each value should be printed on a separate line with  digits after the decimal. The function should not return a value.
    
    Input Format
    
    The first line contains an integer, , the size of the array.
    The second line contains  space-separated integers that describe .
    
    Constraints
    
    
    
    Output Format
    
    Print the following  lines, each to  decimals:
    
    proportion of positive values
    proportion of negative values
    proportion of zeros
    Sample Input
    
    STDIN           Function
    -----           --------
    6               arr[] size n = 6
    -4 3 -9 0 4 1   arr = [-4, 3, -9, 0, 4, 1]
    Sample Output
    
    0.500000
    0.333333
    0.166667
    Explanation
    
    There are  positive numbers,  negative numbers, and  zero in the array.
    The proportions of occurrence are positive: , negative:  and zeros: .

---
+++ plus_minus.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the plusMinus function below.
+void plusMinus(vector<int> arr) {
+
+    float size = arr.size();
+    float neg_count=0;
+    float pos_count=0;
+    float zero_count=0;
+    
+    float neg_ratio=0.000000;
+    float pos_ratio=0.000000;
+    float zero_ratio=0.000000;
+    
+    for(int i=0; i<size; i++){
+        if(arr[i]<0){
+            neg_count++;
+        } else if(arr[i]>0){
+            pos_count++;
+        } else {
+            zero_count++;
+        }
+    }
+    
+    pos_ratio = pos_count/size;
+    cout<< pos_ratio;
+    cout << "\n";
+    
+    neg_ratio = neg_count/size;
+    cout << neg_ratio;
+    cout << "\n";
+    
+    zero_ratio = zero_count/size;
+    cout<< zero_ratio;
+    cout << "\n";
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    plusMinus(arr);
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
